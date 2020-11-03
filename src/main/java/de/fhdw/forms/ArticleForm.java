5f48e85d7dabf030dad5a3772d0af2b55dab12ec
Christoph Mirco Müller <mirco_christoph.mueller@edu.fhdw.de>
2020-11-03 23:33:29 +0100

    removed unused imports

--- src/main/java/de/fhdw/forms/ArticleForm.java
+++ src/main/java/de/fhdw/forms/ArticleForm.java
@@ -1,25 +1,17 @@
 package de.fhdw.forms;
 
-import de.fhdw.endpoints.ArticleImpl;
 import de.fhdw.models.Article;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.tika.detect.Detector;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
-import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
 
 public class ArticleForm {
-    private static final Logger LOG = Logger.getLogger(ArticleForm.class);
 
     @FormParam("Article")
     @PartType(MediaType.APPLICATION_JSON)
